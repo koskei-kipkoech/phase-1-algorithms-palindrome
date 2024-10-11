4f8876152bf237efdbcbd50cc9ce22bbdbe96b64
Patrick Kipkoech <patrickwayy@gmail.com>
2024-10-11 17:09:17 +0300

    Updates on index.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLocaleLowerCase();
+  let palindrome = word.split('').reverse().join('')
+  //return word === palindrome;
+  if (word === palindrome){
+    return true;
+  }else{
+    return false;
+  }
 }
+console.log(isPalindrome('abba'));
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('a'));
+console.log(isPalindrome('robot'));
+console.log(isPalindrome('ab'));
+
 
 /* 
   Add your pseudocode here
-*/
-
+  - define a function
+  - promt a user to input a word or enter the input when calling the function.
+  - convert the word to lowercase
+  - perform the reverse of the word
+  - test if the orginal word is the same with the reversed word using === operator
+  - return true and false 
+*/ 
+  
 /*
   Add written explanation of your solution here
+  - first i ill prompt a user to input a word.
+  - i converted the word to lower case.
+  - performed the reverse of the word using reverse()
+  - tested if the origina word is the same  with the reversed word using the if and === operator. 
+  - return an output. 
+
 */
 
 // You can run `node index.js` to view these console logs
